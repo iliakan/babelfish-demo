4488139699d3c32fbc2063aa3ecf9c756cbf1d3f
Ilya Kantor <iliakan@gmail.com>
2015-12-29 17:13:31 +0300

    ok

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ var doc = yaml.safeLoad(fs.readFileSync('./ru.yml', 'utf8'));
   
 i18n.addPhrase('ru', '', doc);
 
-console.log(i18n.t('ru', 'demo.apples', {count: 3}));
+console.log(i18n.t('ru', '.demo.apples', {count: 3}));
